80694175bbcd332f8049f21b777a20f5112b4bd5
Daniel Salazar <danielsalazar1109@gmail.com>
2024-07-16 18:56:14 -0700

    footer images added

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,4 +1,4 @@
-fetch("./components/footer/foooter.html")
+fetch("./components/footer/footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.querySelector("#footer").innerHTML = data;
