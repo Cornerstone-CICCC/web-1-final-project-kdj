3bfa5af9218a14e1e5aa6127151734098f444fda
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-07-21 20:27:00 -0700

    update: modal popup diy beauty corner section on homepage html

--- homepage.js
+++ homepage.js
@@ -1,3 +1,65 @@
+const firstVideoButton = document.querySelector(".video1");
+const secondVideoButton = document.querySelector(".video2");
+const thirdVideoButton = document.querySelector(".video3");
+
+const modal = document.querySelector(".youtube-container");
+
+const firstVideoClick = firstVideoButton.addEventListener("click", () => {
+  modal.innerHTML = `
+  <div class="youtube">
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/aJ7N3lzHlIY?si=930KAPvoQTxvVqrJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+  `;
+
+  modal.style.display = "flex";
+  modal.style.justifyContent = "center";
+  // modal.backgroundColor = "rgba(255, 255, 255, .8)";
+
+  setTimeout(() => {
+    modal.style.opacity = "1";
+  }, 50);
+});
+
+const secondVideoClick = secondVideoButton.addEventListener("click", () => {
+  modal.innerHTML = `
+  <div class="youtube">
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/hvcp56MpEiE?si=ZHr1sJavA0Q3OcWD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+  `;
+
+  modal.style.display = "flex";
+  modal.style.justifyContent = "center";
+  // modal.backgroundColor = "rgba(255, 255, 255, .8)";
+
+  setTimeout(() => {
+    modal.style.opacity = "1";
+  }, 50);
+});
+
+const thirdVideoClick = thirdVideoButton.addEventListener("click", () => {
+  modal.innerHTML = `
+  <div class="youtube">
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/C47K1TX9PAA?si=9S7HzmRYpJMixrQb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+  `;
+
+  modal.style.display = "flex";
+  modal.style.justifyContent = "center";
+  // modal.backgroundColor = "rgba(255, 255, 255, .8)";
+
+  setTimeout(() => {
+    modal.style.opacity = "1";
+  }, 50);
+});
+
+modal.addEventListener("click", () => {
+  modal.innerHTML = "";
+  modal.style.opacity = "0";
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 450);
+});
+
 function toggleSelect(button) {
   document
     .querySelectorAll(".eventButton")
